4703171f3f8ec7d27d0b2708503a90b91731b6f1
Sunjiv Soni <sanjiv1994@gmail.com>
2016-03-08 15:19:57 +0530

    Basic implementation for two way exchange done

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 import os
 import socket
 
-# Create socket obj
+# Create socket obj for TCP/IP
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
@@ -13,6 +13,7 @@ port = 9999
 # Connection to hostname
 clientSocket.connect((host,port))
 
+clientSocket.send("Foo Bar")
 # Recieve max 1024 bytes
 time = clientSocket.recv(1024)
 
--- server.py
+++ server.py
@@ -1,9 +1,33 @@
 #Server.py
+'''
+class Socket:
+    def __init(self, socket=None):
+        if socket is None:
+            self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        else:
+            self.socket = socket
+
+    def connect(self, host, port):
+        self.sock.connect((host,port))
+
+    def send(self, message):
+        totalSent = 0
+        while totalSent < MSGLEN:
+            sent = self.socket.send(msg[totalSent:])
+            if sent == 0:
+                raise RuntimeError("Socket Connection Broken")
+            totalSent = totalSent + sent
+
+    def recieve(self):
+        chunks
+'''
 
 import socket
 import time
 
-#create a socket object
+# Create a socket object
+# AF_INET - Family
+# SOCK_STREAM - Protocal
 serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # get local machine name
@@ -14,14 +38,18 @@ port = 9999
 #bind to port
 serverSocket.bind((host,port))
 
-# limit 5 requests
+# limit 5 requests at run time
 serverSocket.listen(5)
 
 while True:
     # Establish Connection
-    clientSocket,addr = serverSocket.accept()
+    # Tuple is returned
+    # Waiting till connection is established
+    (clientSocket,(ip,port)) = serverSocket.accept()
 
-    print("Got a connection from %s" %str(addr))
+    print("Got a connection from %s" %str(ip))
+    data = clientSocket.recv(1024)
+    print("Got some data from client %s" %data)
     currentTime = time.ctime(time.time())+"\r\n"
     clientSocket.send(currentTime.encode('ascii'))
     clientSocket.close()
