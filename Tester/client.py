a0016f71e4fa07adfbd39747fd8c28edae7a6b9a
bhatmeister <bhatshubhs12@gmail.com>
2016-03-23 18:21:25 +0530

    Few improvements

--- Tester/client.py
+++ Tester/client.py
@@ -1,8 +1,8 @@
 import socket
 
 s = socket.socket()
-host = '192.168.1.102'# ip of raspberry pi
-port = 12000
+host = '192.168.1.108'# ip of raspberry pi
+port = 12345
 s.connect((host, port))
 print(s.recv(1024))
 s.close()
