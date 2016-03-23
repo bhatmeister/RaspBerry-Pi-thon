dab1bd7f1751ef6b60da7bb3997c944978c4fca7
abhishekdec1994 <abhisheksachdeva@Abhisheks-MacBook-Pro.local>
2016-03-23 23:49:41 +0530

    Add label refresh functionality

--- frontEnd.py
+++ frontEnd.py
@@ -61,8 +61,10 @@ class Weather(Page):
                 Status.place(x=300, y=200)
                 Humidity=tk.Label(self, text="Humidity: "+Weather_Report[2], font=("Helvetica",20))
                 Humidity.place(x=100, y=240)
-                quote = """\n"""+Weather_Report[1]+"""\nHumidity is """+Weather_Report[2]+"""\n"""+Weather_Report[3]
-            #T.insert(tk.END,quote)
+                Weather_Report[3]=Weather_Report[3].split('^')
+                Temp.destroy()
+                Status.destroy()
+                Humidity.destroy()        
         button5 = tk.Button(self, text="Go", bg="Black",fg="White", width=10, command=lambda: sendReq(Locn.get()))
         button5.place(x=300,y=85)
         Locn.place(x=245,y=27)
