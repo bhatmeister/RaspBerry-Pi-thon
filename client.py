04809eccc12c8767c0023176b8bb218c6d3c304e
bhatmeister <bhatshubhs12@gmail.com>
2016-03-23 16:11:52 +0530

    Fix error in exception handling

--- client.py
+++ client.py
@@ -6,9 +6,6 @@ import socket
 client = socketClass.Socket()
 client.timeout(5)
 def createSocket(serverIP,serverPort):
-    print "client socket created"
-    client.connect(serverIP,config.clientPort)
-    print "connection requested"
     try:
         client.connect(serverIP,config.clientPort)
         return 1
