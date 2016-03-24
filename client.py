bf26e4573e7bd52b035a9476141c4d2a5caa4d1c
Sunjiv Soni <sanjiv1994@gmail.com>
2016-03-24 13:26:23 +0530

    Change Server Timeout

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ def createSocket():
     try:
         client = socketClass.Socket()
         print "socket created"
-        client.timeout(5)
+        client.timeout(50)
         return client
 
     except socket.error as socketerror:
