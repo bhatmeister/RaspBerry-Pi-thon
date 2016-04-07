ee058e6a0b20e0c1b13d7f7e7c610cc23ed8bc8a
abhishekdec1994 <abhisheksachdeva@Abhisheks-MacBook-Pro.local>
2016-04-07 12:26:30 +0530

    misc

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ clientPort = 12345
 
 # ServerIP
 
-serverIP = '192.168.1.38'
+serverIP = '100.96.71.201'
--- serverFrontEnd.py
+++ serverFrontEnd.py
@@ -1,22 +1,25 @@
 import Tkinter as tk
-      
+
 root = tk.Tk()
     
 root.minsize(675 ,415)
 root.maxsize(675,415)
 
+
+ 
 label = tk.Label( text="Welcome to raspberry.py server", font=("Helvetica",28))
 label.place(x=115, y=25)
 
 label = tk.Label( text="Server IP:", font=("Helvetica",16))
 label.place(x=220, y=100)
 
-text = tk.Text(root, height=2, width=30,font=("Helvetica",16))
-text.place(x=300, y=100)
-text.insert(tk.END, "Just a text Widget\n")
+IP = tk.Text(root, height=2, width=30,font=("Helvetica",16))
+IP.place(x=300, y=100)
+IP.insert(tk.END, "The server is not live yet")
+IP.config(state=tk.DISABLED)
 
-label = tk.Label( text="Client IP", font=("Helvetica",16))
-label.place(x=300, y=160)
+live = tk.Button( text="LIVE", font=("Helvetica",16))
+live.place(x=300, y=130)
 
 text = tk.Text(root, height=2, width=30,font=("Helvetica",16))
 text.place(x=270, y=210)
@@ -38,5 +41,4 @@ text = tk.Text(root, height=2, width=30,font=("Helvetica",16))
 text.place(x=270, y=330)
 text.insert(tk.END, "Just a text Widget\n")
 
-   
 root.mainloop() 
