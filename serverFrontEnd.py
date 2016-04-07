a555d02957cc4e8f648361e3aa5bf7231259227e
abhishekdec1994 <abhisheksachdeva@Abhisheks-MacBook-Pro.local>
2016-04-07 13:56:53 +0530

    call function for server live button

--- serverFrontEnd.py
+++ serverFrontEnd.py
@@ -1,11 +1,14 @@
 import Tkinter as tk
+from concurrentServer import *
 
 root = tk.Tk()
     
 root.minsize(675 ,415)
 root.maxsize(675,415)
 
-
+def serveLive():
+    makeServerLive(IP,live)
+    
  
 label = tk.Label( text="Welcome to raspberry.py server", font=("Helvetica",28))
 label.place(x=115, y=25)
@@ -18,7 +21,7 @@ IP.place(x=300, y=100)
 IP.insert(tk.END, "The server is not live yet")
 IP.config(state=tk.DISABLED)
 
-live = tk.Button( text="LIVE", font=("Helvetica",16))
+live = tk.Button( text="LIVE", font=("Helvetica",16),command=lambda: serveLive(IP,live))
 live.place(x=300, y=130)
 
 text = tk.Text(root, height=2, width=30,font=("Helvetica",16))
