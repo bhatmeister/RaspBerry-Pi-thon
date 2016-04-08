be0bcbc4cd26a7aacf427170020081848d0147ee
bhatmeister <bhatshubhs12@gmail.com>
2016-04-08 16:56:00 +0530

    Fix yet another encode error
    The nightmare still continues.

--- fetchData.py
+++ fetchData.py
@@ -16,7 +16,7 @@ def dataFetcher(type,data):
     "This function acts as a switch case for switching between type of data to be fetched"
     rData = ""
     if type == '0':
-        rData = fetchWeather(data)
+        rData = fetchWeather(data).encode('utf8')
     elif type == '1':
         rData = fetchNews().encode('utf8')
     return rData
